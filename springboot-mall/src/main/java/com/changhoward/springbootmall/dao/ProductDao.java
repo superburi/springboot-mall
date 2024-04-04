9247518ffdf8346bccde8c38c4735b39cc273383
CYH <howardcyh@icloud.com>
2024-04-05 00:43:11 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- springboot-mall/src/main/java/com/changhoward/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/changhoward/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.changhoward.springbootmall.dao;
 import com.changhoward.springbootmall.dto.ProductRequest;
 import com.changhoward.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/changhoward/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/changhoward/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.changhoward.springbootmall.service;
 import com.changhoward.springbootmall.dto.ProductRequest;
 import com.changhoward.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
