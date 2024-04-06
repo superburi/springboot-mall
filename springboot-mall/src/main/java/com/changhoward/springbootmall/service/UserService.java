8c8d14e8bec68a5fc27ed37fc74fd3415739995d
CYH <howardcyh@icloud.com>
2024-04-06 17:54:11 +0800

    帳號功能 - 登入

--- springboot-mall/src/main/java/com/changhoward/springbootmall/service/UserService.java
+++ springboot-mall/src/main/java/com/changhoward/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.changhoward.springbootmall.service;
 
+import com.changhoward.springbootmall.dto.UserLoginRequest;
 import com.changhoward.springbootmall.dto.UserRegisterRequest;
 import com.changhoward.springbootmall.model.User;
 
@@ -8,5 +9,7 @@ public interface UserService {
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 
 }
